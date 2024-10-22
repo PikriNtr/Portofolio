36ca871fe7540c4944512aabb55123ab0b2319bb
PikriNtr <fikrihaikal2101@gmail.com>
2024-10-22 20:12:04 +0800

    fixing: rebuild hover stuff

--- src/components/HoverAnother.tsx
+++ src/components/HoverAnother.tsx
@@ -1,27 +1,40 @@
 import { useState, useRef, useEffect } from "react";
 
-const HoverImageVideo4 = () => {
-    const [setIsHovered, isHovered] = useState(false);
-    const videoRef = useRef<HTMLVideoElement>(null);
-    const handleClick = () => {
-        window.location.href = 'https://'
+const HoverImageVideo5 = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const handleClick = () => {
+    window.location.href = "https://www.instagram.com/reel/C9jtvgqP7CJ";
+  };
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.controls = false;
     }
+  }, []);
 
-    useEffect(() {
-        if (videoRef.current) {
-            videoRef.current.controls = false;
-        }
-    }, []);
+  return (
+    <div
+      className="w-full max-w-lg h-64 relative overflow-hidden"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onClick={handleClick}
+    >
+      {!isHovered ? (
+        <img
+          src="/yrlplyz.png" 
+          alt="Thumbnail"
+        />
+      ) : (
+        <video
+          ref={videoRef}
+          src="/yrlplyz.mp4" 
+          autoPlay
+          loop
+        />
+      )}
+    </div>
+  );
+};
 
-    return (
-        <div 
-        className="w-full max-w-lg h-64 relative overflow-hidden"
-        onMouseEnter={() => setIsHovered(True)}
-        onMouseLeave={() => setIsHovered(False)}
-        onClick={handleClick}
-        
-        >
-            
-        </div>
-    )
-}
+export default HoverImageVideo5;
